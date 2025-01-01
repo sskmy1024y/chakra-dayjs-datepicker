a0b07ca14443a09b202b40a802b8707c525c75bf
sho <sskmy1024.y@gmail.com>
2025-01-01 23:06:33 +0900

    fix gh-page directory

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/chakra-dayzed-datepicker/",
+  base: "/chakra-dayjs-datepicker/",
   plugins: [react()],
   server: {
     fs: {
-      allow: ['..']
-    }
-  }
+      allow: [".."],
+    },
+  },
 });
